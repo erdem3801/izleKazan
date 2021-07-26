2979cbe7c58b296a32496b8132e9a6c157add4ce
erdem3801 <burkaerdem@gmail.com>
2021-07-26 23:56:28 +0300

    tr sidebar güncellendi

--- app/Language/tr/sidebar.php
+++ app/Language/tr/sidebar.php
@@ -3,5 +3,28 @@
 // override core en language system validation or define your own en language validation message
 return [
     'siteName' => 'Sİteni adı',
-    'title'     => 'site başlığı'
+    'title'     => 'site başlığı',
+    'menu1' => 'Kontrol Paneli',
+    'menuKesici' => 'içerik',
+    'dropDownReklam'=>'Reklamlar',
+    'menuReklamKesici' => 'Reklamlar Ve Yardımcı Programlamlar',
+    'menuReklam1' => 'Reklamlarım',
+    'menuReklam2' => 'Reklam Ekle',
+    'menuReklam3' => 'Bütçe',
+    'dropDownReferanslar' => 'Referanslar',
+    'referansKesici' => 'Referans Araçları',
+    'menuReferans1' => 'İstatislik',
+    'menuReferans2' => 'Yapı',
+    'menuReferans3' => 'Promosyon Afişleri',
+    'menuReferans4' => 'UTM etiketleri',
+    'dropDownUser' => 'Kullanıcı',
+    'kullanıcıKesici' =>' Kullanıcı Yardımcı Programlar',
+    'userMenu1' => 'Ayarlar',
+    'userMenu2' => 'Para Çekme Geçmişi',
+    'dropDownHelp' => 'Yardım',
+    'yardımKesici' => 'Yardım Araçları',
+    'helpMenu1' => 'SSS',
+    'helpMenu2' => 'Destek'
+
+
 ];
