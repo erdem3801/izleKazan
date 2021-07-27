19a049d95ec03d29ee74c9eae6167f346a150797
erdem3801 <burkaerdem@gmail.com>
2021-07-27 23:22:45 +0300

    add adverts tasarlanmaya başladı

---
+++ app/Views/addAdverts.php
@@ -0,0 +1,149 @@
+<?= $this->extend('template/layout')  ?>
+<?= $this->section('content')  ?>
+
+
+<div class="main">
+    <div class="container">
+        <div id="tabpages" class="tab-list">
+            TAB PAGES
+        </div>
+        <div class="container-fluid  ">
+            <div class="row">
+                <div class=" col-xl-5 col-sm-12 border ">
+                    <a href="">
+                        <div class="row">
+                            <div class="col col-sm-6 border">
+                                <i class="fab fa-youtube fa-5x center"></i>
+
+                            </div>
+                            <div class="col col-sm-6">
+                                <div class="title">
+                                    Youtube Advert
+                                </div>
+                                <div class="icerik">
+                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
+                                    <br><br>
+                                    <ul style="font-size: 12px; list-style-type: none; color:black;">
+                                        <li>
+                                            <span>
+                                                Potential reach:
+                                            </span>
+                                            158154515
+
+                                        </li>
+                                        <li>
+                                            Price from:
+                                            <span> $1.00</span>
+                                            per 1000 views
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class=" col-xl-2  ">
+                </div>
+                <div class=" col-xl-5 border ">
+                    <a href="">
+                        <div class="row ">
+                            <div class="col border">
+                                <i class="fab fa-file-text-o"></i>
+
+                            </div>
+                            <div class="col">
+                                <div class="title">
+                                    Multi size advert
+
+                                </div>
+                                <div class="icerik">
+                                    <p> Specify a title, description, and upload any image. The system will generate banners of different sizes, which will be shown in the Surfe.pro advertising network, as well as to users who have installed the browser extension.
+                                    </p>
+                                    <br>
+                                    <ul style="font-size: 12px; list-style-type: none; color:black;">
+                                        <li>
+                                            <span>Sizes:</span>
+
+                                            468x60, 728x90, 240x400, 300x250 and other
+
+                                        </li>
+                                        <li>
+                                            <span>
+                                                Daily reach:
+                                            </span>
+
+                                            823 972
+
+                                        </li>
+                                        <li>
+                                            Price from:
+                                            <span>
+                                                $0.04
+                                            </span>
+                                            per 1000 impressions or
+                                            <span>
+                                                $0.02 per click
+                                            </span>
+                                        </li>
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="container-fluid ">
+            <div class="row">
+                <div class=" col-xl-5 border ">
+                    <a href="">
+                        <div class="row">
+                            <div class="col border">
+                                <i class="fab fa-youtube fa-5x center"></i>
+
+                            </div>
+                            <div class="col">
+                                <div class="title">
+                                    3
+                                </div>
+                                <div class="icerik">
+                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class=" col-xl-2  ">
+                </div>
+                <div class=" col-xl-5 border ">
+                    <a href="">
+                        <div class="row">
+                            <div class="col border">
+                                <i class="fab fa-youtube fa-5x center"></i>
+
+                            </div>
+                            <div class="col">
+                                <div class="title">
+                                    4 </div>
+                                <div class="icerik">
+                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</div>
+
+
+<?= $this->endSection()  ?>
