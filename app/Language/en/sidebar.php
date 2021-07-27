ca2f3a27c5f25409ce35576aa07af01ad3465561
erdem3801 <burkaerdem@gmail.com>
2021-07-27 20:33:31 +0300

    Sidebar EN tamamlandı

--- app/Language/en/sidebar.php
+++ app/Language/en/sidebar.php
@@ -2,6 +2,29 @@
 
 // override core en language system validation or define your own en language validation message
 return [
-    'siteName' => 'IZLE KAZAN EN'
-    
+    'siteName' => 'Site Name',
+    'title'     => 'Site Title',
+    'menu1' => 'Control Panel',
+    'menuKesici' => 'Contents',
+    'dropDownReklam' => 'Ads',
+    'menuReklamKesici' => 'Advertising Tools',
+    'menuReklam1' => 'My Ads',
+    'menuReklam2' => 'Add Ad',
+    'menuReklam3' => 'Budget',
+    'dropDownReferanslar' => 'Referances',
+    'referansKesici' => 'Referance Tools',
+    'menuReferans1' => 'Statistics',
+    'menuReferans2' => 'Buidl',
+    'menuReferans3' => 'Promotional Posters',
+    'menuReferans4' => 'UTM Tags',
+    'dropDownUser' => 'User ',
+    'kullanıcıKesici' => ' User Tools',
+    'userMenu1' => 'Settings',
+    'userMenu2' => 'Withdrawal History',
+    'dropDownHelp' => 'Help',
+    'yardımKesici' => 'Help Tools',
+    'helpMenu1' => 'SSS',
+    'helpMenu2' => 'Support'
+
+
 ];
