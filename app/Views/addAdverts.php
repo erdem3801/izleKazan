680b6d2ed7c5d1205b7c9718b543b8c3335eb98b
erdem3801 <burkaerdem@gmail.com>
2021-07-31 15:55:15 +0300

    addavets düzeb

--- app/Views/addAdverts.php
+++ app/Views/addAdverts.php
@@ -7,23 +7,27 @@
         <div id="tabpages" class="tab-list">
             TAB PAGES
         </div>
+
         <div class="container-fluid  ">
             <div class="row">
-                <div class=" col-xl-5 col-sm-12 border ">
+                <div class=" col-xl-6 col-sm-12 border addverts ">
                     <a href="">
                         <div class="row">
-                            <div class="col col-sm-6 border">
-                                <i class="fab fa-youtube fa-5x center"></i>
+                            <div class="col col-sm-6   ">
+
+                                <i class=" fa-5x fab fa-youtube addadverts-ico " style="padding-left:28%; padding-right:5%; padding-top:40%;"></i>
 
                             </div>
-                            <div class="col col-sm-6">
-                                <div class="title">
-                                    Youtube Advert
-                                </div>
-                                <div class="icerik">
-                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
-                                    <br><br>
-                                    <ul style="font-size: 12px; list-style-type: none; color:black;">
+                            <div class="col col-sm-6 textler ">
+                                <div class="textler-about">
+                                    <div class="title">
+                                        Youtube Advert
+                                    </div>
+                                    <div class="icerik">
+                                        Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
+                                    </div>
+                                    <br>
+                                    <ul>
                                         <li>
                                             <span>
                                                 Potential reach:
@@ -42,25 +46,26 @@
                         </div>
                     </a>
                 </div>
-                <div class=" col-xl-2  ">
-                </div>
-                <div class=" col-xl-5 border ">
+
+                <div class=" col-xl-6 col-sm-12 border addverts ">
                     <a href="">
                         <div class="row ">
-                            <div class="col border">
-                                <i class="fab fa-file-text-o"></i>
-
+                            <div class="col ">
+                                <i class=" fa-5x fa fa-list-alt addadverts-ico " style="padding-left:28%; padding-right:5%; padding-top:40%;"></i>
                             </div>
-                            <div class="col">
-                                <div class="title">
-                                    Multi size advert
+                            <div class="col col-sm-6 textler ">
+                                <div class="textler-about">
+                                    <div class="title">
+                                        Multi size advert
 
-                                </div>
-                                <div class="icerik">
-                                    <p> Specify a title, description, and upload any image. The system will generate banners of different sizes, which will be shown in the Surfe.pro advertising network, as well as to users who have installed the browser extension.
-                                    </p>
+                                    </div>
+                                    <div class="icerik">
+                                        <p> Specify a title, description, and upload any image. The system will generate banners of different sizes, which will be shown in the Surfe.pro advertising network, as well as to users who have installed the browser extension.
+                                        </p>
+                                    </div>
+
                                     <br>
-                                    <ul style="font-size: 12px; list-style-type: none; color:black;">
+                                    <ul>
                                         <li>
                                             <span>Sizes:</span>
 
@@ -95,48 +100,85 @@
                 </div>
             </div>
         </div>
-        <br>
+
         <div class="container-fluid ">
             <div class="row">
-                <div class=" col-xl-5 border ">
+                <div class=" col-xl-6 col-sm-12 border addverts ">
                     <a href="">
                         <div class="row">
-                            <div class="col border">
-                                <i class="fab fa-youtube fa-5x center"></i>
-
+                            <div class="col ">
+                                <i class=" fa-5x fa fa-window-maximize addadverts-ico " style="padding-left:28%; padding-right:5%; padding-top:40%;"></i>
                             </div>
-                            <div class="col">
-                                <div class="title">
-                                    3
-                                </div>
-                                <div class="icerik">
-                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
+                            <div class="col col-sm-6 textler ">
+                                <div class="textler-about">
+                                    <div class="title">
 
+                                        POPUP
+                                    </div>
+                                    <div class="icerik">
+                                        People going be transferred to the site with a&nbsp;guaranteed time of stay on&nbsp;the page. Live traffic source is the browser extension and the&nbsp;mobile application.
+                                    </div>
+<br>
+                                    <ul>
+                                        <li>
+                                            <span>
+                                                Daily reach:
+                                            </span>
+                                            32 154
+                                        </li>
+                                        <li>
+                                            Price from: <span>$1.00</span> per 1000 visits
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class=" col-xl-2  ">
-                </div>
-                <div class=" col-xl-5 border ">
-                    <a href="">
+
+                <div class=" col-xl-6 col-sm-12 border addverts "> <a href="">
                         <div class="row">
-                            <div class="col border">
-                                <i class="fab fa-youtube fa-5x center"></i>
-
+                            <div class="col ">
+                                <i class=" fa-5x  fa fa-picture addadverts-ico " style="padding-left:28%; padding-right:5%; padding-top:40%;"></i>
                             </div>
-                            <div class="col">
-                                <div class="title">
-                                    4 </div>
-                                <div class="icerik">
-                                    Specify a&nbsp;link to the video in Youtube and a&nbsp;link to the site to go. Our users will watch your video and then be redirected to&nbsp;your site.
-
+                            <div class="col col-sm-6 textler ">
+                                <div class="textler-about">
+                                    <div class="title">
+                                        Banner advert </div>
+                                    <div class="icerik">
+                                        Upload graphic banners in standard sizes. They will be shown in the Surfe.pro advertising network, as well as to users who have installed the browser extension.
+                                    </div>
+<br>
+                                    <ul>
+                                        <li>
+                                            <span>
+                                                Sizes:
+                                            </span>
+                                            468x60, 728x90, 240x400, 300x250
+                                        </li>
+                                        <li>
+                                            <span>
+                                                Daily reach:
+                                            </span>
+                                            733 546
+                                        </li>
+                                        <li>
+                                            Price from:
+                                            <span>
+                                                $0.04
+                                            </span>
+                                            per 1000 impressions or
+                                            <span>
+                                                $0.02 per click
+                                            </span>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </a>
                 </div>
+
             </div>
         </div>
 
