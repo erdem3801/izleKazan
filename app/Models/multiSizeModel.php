bf647e541315b92d28109640da1c98ba9b37c1d7
erdem3801 <burkaerdem@gmail.com>
2021-07-31 10:59:36 +0300

    multisizemodel

---
+++ app/Models/multiSizeModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class multiSizeModel extends Model
+{
+    protected $table      = 'multiSizeTable';
+    protected $primaryKey = 'ID';
+
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = [
+        'multiSizeImg',
+        'multiSizeTittle',
+        'multiSizeUrl',
+        'multiSizeText',
+        'multiSizePos',
+        'multiSizeHit',
+        'multiSizeHitMoney',
+        'multiSizeMoney',
+        'multiSizeHrefUrl'
+
+    ];
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
